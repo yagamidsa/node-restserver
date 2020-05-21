7284f513cb954e5d6181011f18ac64770175621c
David Alipio <yagamidsa@hotmail.com>
2020-05-20 19:19:08 -0500

    update restserver

--- server/config/config.js
+++ server/config/config.js
@@ -22,12 +22,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 ///Entorno
 ///============================================
 
-// let urlDB;
+let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://yagamidsa:Lxc19dXlsi4QpvKs@cafe-zscnd.mongodb.net/cafe?retryWrites=true&w=majority';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://yagamidsa:Lxc19dXlsi4QpvKs@cafe-zscnd.mongodb.net/cafe?retryWrites=true&w=majority';
+}
 
 process.env.URLDB = urlDB;
